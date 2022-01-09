27c3ee40004ff151396a0303f4c636a6e68b23fb
Pauloamm <59803151+Pauloamm@users.noreply.github.com>
2022-01-09 17:05:20 +0000

    Project Moved to Repository

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.U2D.Path;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Random = System.Random;
+
+public class Movement : MonoBehaviour
+{
+
+    public delegate void OnDestroy();
+    public event OnDestroy OnDestroyEvent;
+    
+    struct Wisker
+    {
+        public Vector3 direction;
+        public float distanceToObject;
+    }
+
+   
+    // w1a + w2b + w3c + w4ab + w5ac + w6bc + w7a2 + w8b2 + w9c2 = angle to turn 
+    //private const int numberOfWeights = 9;
+    
+    //Weights
+    public float[] wiskersWeights;
+        
+        
+        
+    private float timeAlive = 0f;
+    public float GetAliveTime => timeAlive;
+    
+    public float speed = 1f;
+    
+    public float maxAngle = 90f;
+    public float wiskerSize = 0.5f;
+
+    private Dictionary<char, Wisker> wiskersDict;
+    
+    
+   
+    //Wiskers
+    private Wisker frontWiskerDirection;
+    private Wisker leftWiskerDirection;
+    private Wisker rightWiskerDirection;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // wiskersWeights = new float[9];
+        // for (int index = 0; index < wiskersWeights.Length; index++)
+        // {
+        //     //wiskersWeights[index] = 1;
+        //     Random rg = new Random();
+        //     wiskersWeights[index] = (float)rg.NextDouble()*2f -1;
+        // }
+
+       
+        
+        
+        wiskersDict = new Dictionary<char, Wisker>();
+        UpdateWiskers();
+        
+        wiskersDict.Add('a',leftWiskerDirection);
+        wiskersDict.Add('b',frontWiskerDirection);
+        wiskersDict.Add('c',rightWiskerDirection);
+
+    }
+    void Update()
+    {
+        timeAlive += Time.deltaTime;
+        
+        MovePosition();
+        UpdateWiskers();
+        
+        
+        transform.up = Quaternion.Euler(0, 0, AngleToTurn()) * transform.up;
+    }
+
+    void MovePosition()
+    {
+        this.transform.position += transform.up * Time.deltaTime * speed;
+    }
+
+    void UpdateWiskers()
+    {
+        frontWiskerDirection.direction =  transform.up;
+        leftWiskerDirection.direction =  (Quaternion.Euler(0, 0, maxAngle / 2) * transform.up)  ;
+        rightWiskerDirection.direction =  (Quaternion.Euler(0, 0, -maxAngle / 2) * transform.up) ;
+    }
+
+
+    float SendRaycast(Vector3 wiskerToRaycast)
+    {
+        RaycastHit hit;
+
+        if (!Physics.Raycast(transform.position, wiskerToRaycast, out hit, wiskerSize)) return 0;
+        
+        return hit.distance;
+        
+ 
+    }
+
+    
+    
+     float AngleToTurn()
+     {
+         float a = SendRaycast(wiskersDict['a'].direction);
+         float b = SendRaycast(wiskersDict['b'].direction);
+         float c = SendRaycast(wiskersDict['c'].direction);
+
+
+         return wiskersWeights[0] * a +
+                wiskersWeights[1] * b +
+                wiskersWeights[2] * c +
+                wiskersWeights[3] * a * b +
+                wiskersWeights[4] * a * c +
+                wiskersWeights[5] * b * c +
+                wiskersWeights[6] * a * a +
+                wiskersWeights[7] * b * b +
+                wiskersWeights[8] * c * c ;
+         
+     }
+    
+     private void OnTriggerEnter(Collider other)
+     {
+         if (!other.CompareTag("Wall")) return;
+         
+         OnDestroyEvent?.Invoke();
+         gameObject.SetActive(false);
+     }
+     
+     //DEBUG----------------------------------------------------
+     
+     
+     bool isObjectHittingWisker(Vector3 wiskerDirection)
+     {
+         float currentDist = SendRaycast(wiskerDirection);
+
+         return currentDist > 0 && currentDist < wiskerSize;
+
+     }
+    private void OnDrawGizmos()
+    {
+        Vector3 currentPosition = this.transform.position;
+        
+        if(isObjectHittingWisker(frontWiskerDirection.direction)) Gizmos.color = Color.red;
+        else Gizmos.color = Color.green;
+        Gizmos.DrawLine(currentPosition, currentPosition + frontWiskerDirection.direction * wiskerSize);
+
+        if(isObjectHittingWisker(leftWiskerDirection.direction)) Gizmos.color = Color.red;
+        else Gizmos.color = Color.green;
+        Gizmos.DrawLine(currentPosition, currentPosition + leftWiskerDirection.direction* wiskerSize);
+        
+        
+        if(isObjectHittingWisker(rightWiskerDirection.direction)) Gizmos.color = Color.red;
+        else Gizmos.color = Color.green;
+        Gizmos.DrawLine(currentPosition, currentPosition + rightWiskerDirection.direction* wiskerSize);
+
+    }
+
+    public void NormalizeTimeAlive(float totalPopulationTime)
+    {
+        this.timeAlive = this.timeAlive / totalPopulationTime;
+    }
+   
+}
---
+++ Assets/Scripts/PopulationManager.cs
@@ -0,0 +1,258 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using Random = System.Random;
+
+public class PopulationManager : MonoBehaviour
+{
+    public int currentGeneration = 1;
+
+
+    //MUTATION
+    public float mutationProbability = 0.42f;
+
+    struct Parents
+    {
+        public Movement firstParent;
+        public Movement secondParent;
+    }
+
+    public int chromossomeSize = 9;
+
+    public GameObject objectToSpawn;
+    public int populationNumber = 50;
+
+    private List<Movement> population;
+
+
+    public int currentPopulationAlive;
+
+    void GiveRandomChromossomes()
+    {
+        Random rg = new Random();
+
+        for (int i = 0; i < populationNumber; i++)
+        {
+            Movement newGameObject = Instantiate(objectToSpawn).GetComponent<Movement>();
+
+            float[] newChromossome = new float[chromossomeSize];
+
+
+            for (int index = 0; index < newChromossome.Length; index++)
+            {
+                //wiskersWeights[index] = 1;
+                newChromossome[index] = (float) rg.NextDouble() * 2f - 1;
+            }
+
+            newGameObject.wiskersWeights = newChromossome;
+            newGameObject.OnDestroyEvent += UpdateNumberAlive;
+            population.Add(newGameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        currentPopulationAlive = populationNumber;
+        population = new List<Movement>();
+        GiveRandomChromossomes();
+    }
+
+    // Update is called once per frame
+
+    void UpdateNumberAlive()
+    {
+        currentPopulationAlive--;
+        if (currentPopulationAlive == 0)
+        {
+            CreateNewGeneration();
+            currentPopulationAlive = populationNumber;
+        }
+    }
+
+
+    void CreateNewGeneration()
+    {
+        population = population.OrderBy(mov => mov.GetAliveTime).ToList();
+
+        NormalizeAliveTimes();
+
+        List<Parents> selectedParents = TournamentSelection();
+
+        TwoPointCrossover(selectedParents);
+    }
+
+    void NormalizeAliveTimes()
+    {
+        float totalAliveTime = 0;
+        foreach (Movement individual in population) totalAliveTime += individual.GetAliveTime;
+        foreach (Movement individual in population) individual.NormalizeTimeAlive(totalAliveTime);
+    }
+
+    List<Parents> SelectionRoulette()
+    {
+        List<Parents> selectedPairs = new List<Parents>();
+
+
+        Parents tempParents = new Parents();
+
+        while (selectedPairs.Count < populationNumber)
+        {
+            Random rg = new Random();
+            float newNumber = (float) rg.NextDouble();
+
+            float accumulativeProb = 0;
+
+            foreach (Movement individual in population)
+            {
+                accumulativeProb += individual.GetAliveTime;
+
+                if (newNumber > accumulativeProb) continue;
+
+                if (tempParents.firstParent == null)
+                {
+                    tempParents.firstParent = individual;
+                    break;
+                }
+
+                if (tempParents.firstParent == individual) break;
+
+                tempParents.secondParent = individual;
+                selectedPairs.Add(tempParents);
+                tempParents = new Parents();
+                break;
+            }
+        }
+
+
+        return selectedPairs;
+    }
+
+    List<Parents> TournamentSelection()
+    {
+        List<Parents> selectedParents = new List<Parents>();
+
+        int numberOfTournamentParticipants = 3;
+
+        List<Movement> selectedParticipants = new List<Movement>();
+
+        Parents newParentPair = new Parents();
+        do
+        {
+            List<int> selectedIndexes = new List<int>();
+            do
+            {
+                Random rg = new Random();
+
+
+                int randomSelectIndex = rg.Next(0, populationNumber);
+
+                bool alreadySelected = false;
+                foreach (int index in selectedIndexes)
+                {
+                    if (index == randomSelectIndex)
+                        alreadySelected = true;
+                }
+
+                if (!alreadySelected) selectedIndexes.Add(randomSelectIndex);
+                
+                
+            } while (selectedIndexes.Count < numberOfTournamentParticipants);
+            
+            
+            
+            Movement tournamentWinner = null;
+
+            foreach (int index in selectedIndexes)
+            {
+                if (tournamentWinner == null) tournamentWinner = population[index];
+                else if (tournamentWinner.GetAliveTime < population[index].GetAliveTime)
+                    tournamentWinner = population[index];
+            }
+
+            if (newParentPair.firstParent == null) newParentPair.firstParent = tournamentWinner;
+            else if(newParentPair.firstParent != tournamentWinner )
+            {
+                newParentPair.secondParent = tournamentWinner;
+                selectedParents.Add(newParentPair);
+                newParentPair = new Parents();
+            }
+        } while (selectedParents.Count < populationNumber);
+
+
+        return selectedParents;
+    }
+
+    void TwoPointCrossover(List<Parents> selectedParents)
+    {
+        Random rg = new Random();
+        int firstPoint = rg.Next(0, chromossomeSize+1);
+
+
+        int secondPoint;
+        do
+        {
+            secondPoint = rg.Next(0, chromossomeSize+1);
+        } while (secondPoint == firstPoint);
+
+
+        List<Movement> newGeneration = new List<Movement>();
+
+
+        int counterForName = 1;
+        foreach (Parents ps in selectedParents)
+        {
+            float[] newChromossome = new float[chromossomeSize];
+            
+            //CHECK FOR REFERENCE/VALUE TYPE
+            //newChromossome = ps.firstParent.wiskersWeights;
+
+            for (int i = 0; i < chromossomeSize; i++)
+            {
+                newChromossome[i] = ps.firstParent.wiskersWeights[i];
+            }
+
+            for (int index = 0; index < chromossomeSize; index++)
+            {
+                if ((index >= firstPoint && index <= secondPoint) ||
+                    (index <= firstPoint && index >= secondPoint))
+                {
+                    newChromossome[index] = ps.secondParent.wiskersWeights[index];
+                }
+            }
+
+            GameObject newObject = Instantiate(objectToSpawn);
+            newObject.name = "CarNr " + counterForName++;
+
+
+            Movement temp = newObject.GetComponent<Movement>();
+
+
+            if (rg.NextDouble() <= mutationProbability)
+            {
+                newChromossome = MutateChromossome(newChromossome);
+            }
+
+            temp.wiskersWeights = newChromossome;
+            temp.OnDestroyEvent += UpdateNumberAlive;
+            newGeneration.Add(temp);
+        }
+
+        foreach (Movement m in population) Destroy(m.gameObject);
+        population = newGeneration;
+        currentGeneration++;
+    }
+    
+    
+    float[] MutateChromossome(float[] originalChromossome)
+    {
+        Random rg = new Random();
+
+        int geneToMutate = rg.Next(0, chromossomeSize);
+        originalChromossome[geneToMutate] = (float) rg.NextDouble() * 2f - 1;
+
+        return originalChromossome;
+    }
+}
